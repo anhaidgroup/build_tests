d0b5942b20f8da1039934e1efa00cb09e580d438
Paul <paulgc@cs.wisc.edu>
2016-07-18 19:23:04 -0500

    Remove old files

--- test_py_stringmatching.py
+++
@@ -1,8 +0,0 @@
-import py_stringmatching as sm
-print('install path : ' + sm.get_install_path())
-print('version : ' + sm.__version__)
-l = sm.Levenshtein()
-print('edit dist : ' + str(l.get_raw_score('ag', 'dg')))
-q = sm.QgramTokenizer()
-print('prefix pad : ' + q.get_prefix_pad())
-
